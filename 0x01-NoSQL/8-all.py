e4c4393867ef846e30cb5fd4ce6fdeb3b9d8bc25
Nonnyyy <migwilg025@gmail.com>
2023-07-17 16:17:43 +0000

    Commits

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,4 +1,5 @@
- Python function that lists all documents in a collection:
+#!/usr/bin/env python3
+"""Write a Python function that lists all documents in a collection:
 
 Prototype: def list_all(mongo_collection):
 Return an empty list if no document in the collection
